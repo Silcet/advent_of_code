466819a8db092aca5e1b01af18673ee8977669fb
Silcet <camorga1@gmail.com>
2023-12-18 22:09:35 +0100

    Finished day1 2023

--- 2023/day1_23/src/main.rs
+++ 2023/day1_23/src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::collections::HashMap;
 use utils;
 
@@ -13,39 +14,69 @@ fn part_one(input: &String) -> u32 {
         .sum()
 }
 
-fn part_two(input: &String) -> u32 {
-    let digit_map: HashMap<&str, &str> = HashMap::from([
-        ("one", "1"),
-        ("two", "2"),
-        ("three", "3"),
-        ("four", "4"),
-        ("five", "5"),
-        ("six", "6"),
-        ("seven", "7"),
-        ("eight", "8"),
-        ("nine", "9"),
+fn slice_to_number(slice: &str) -> u32 {
+    let digit_map: HashMap<&str, u32> = HashMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
     ]);
+
+    match slice.parse::<u32>() {
+        Ok(digit) => digit,
+        Err(_) => *digit_map.get(slice).unwrap_or(&0),
+    }
+}
+
+fn part_two(input: &String) -> u32 {
+    let digit_regex: Regex =
+        Regex::new(r"\d|one|two|three|four|five|six|seven|eight|nine").unwrap();
 
     input
         .lines()
         .inspect(|l| println!("{l}"))
         .map(|line| {
-            let mut numeric_line = line.to_string();
-            for (text, number) in digit_map.iter() {
-                numeric_line = numeric_line.replace(text, number);
+            let mut first: Option<u32> = None;
+            let mut second: Option<u32> = None;
+
+            for i in 1..=line.len() {
+                if first.is_none() {
+                    let first_slice = &line[0..i];
+                    println!("First slice: {first_slice:?}");
+                    if let Some(first_match) = digit_regex.find(&line[0..i]) {
+                        first = Some(slice_to_number(first_match.as_str()));
+                        println!("First: {}", first.unwrap());
+                    }
+                }
+                if second.is_none() {
+                    let second_slice = &line[line.len() - i..line.len()];
+                    println!("Second slice: {second_slice}");
+                    if let Some(first_match) = digit_regex.find(&line[line.len() - i..line.len()]) {
+                        second = Some(slice_to_number(first_match.as_str()));
+                        println!("Second: {}", second.unwrap());
+                    }
+                }
+                if first.is_some() && second.is_some() {
+                    break;
+                }
             }
-            let mut numbers = numeric_line.chars().filter_map(|c| c.to_digit(10));
-            println!("{numbers:?}");
-            let first = numbers.next().unwrap_or(0);
-            let second = numbers.last().unwrap_or(first);
-            first * 10 + second
+
+            first.unwrap_or(0) * 10 + second.unwrap_or(0)
         })
         .inspect(|n| println!("{n}"))
         .sum()
 }
 
 fn main() {
-    let input = utils::get_day_input(2023, 1).unwrap();
+    let mut aoc = utils::AdventOfCode::new(2023, 1);
+    let input = aoc
+        .get_input()
+        .expect("Failed to get an input for the problem");
 
     let first_result = part_one(&input);
     println!("Part one result: {first_result}");
